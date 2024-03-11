67a0a0caba5567d7f911cff55aa893d86d81c8b1
Sayantan <100552235+Arbtrage@users.noreply.github.com>
2024-03-12 00:30:39 +0530

    Update constants.ts

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,4 @@
 // export const BackendURL = "https://future-layoffs.onrender.com/"
 // export const BackendURL="https://langchain-86zw.onrender.com/"
 
-export const BackendURL ="https://future-layoffs-llm-production.up.railway.app"
+export const BackendURL ="https://future-layoffs-llm-production.up.railway.app/"
